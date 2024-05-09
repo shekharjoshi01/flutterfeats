1261ac953b505216ff056909c44245ecbdc10a70
Mamta Bhatt <610mamtabhatt@gmail.com>
2024-05-09 20:36:23 +0545

    bottom navbar

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryBlue = Color(0xFF0096f8);
+  static const Color secondary = Color(0xFF001b2f);
+
+  static const Color yellow = Color(0xFFFEDC56);
+  static const Color white = Color(0xFFFFFFFF);
+
+
+}
